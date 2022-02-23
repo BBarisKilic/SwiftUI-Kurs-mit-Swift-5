37e02b52c7dbc221dd0ffe566486cb68f9003ff3
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-02-24 00:38:30 +0100

    Created Erste App mit SwiftUI.

---
+++ 01_Erste-App-mit-SwiftUI/01_Erste-App-mit-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  01_Erste-App-mit-SwiftUI
+//
+//  Created by Bülent Barış Kılıç on 24.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 01_Erste-App-mit-SwiftUI/01_Erste-App-mit-SwiftUI/_1_Erste_App_mit_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_Erste_App_mit_SwiftUIApp.swift
+//  01_Erste-App-mit-SwiftUI
+//
+//  Created by Bülent Barış Kılıç on 24.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct _1_Erste_App_mit_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
