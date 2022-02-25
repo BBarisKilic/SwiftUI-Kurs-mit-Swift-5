0fe0d4bd1f45180921d9e1716f56bded5be9f6f4
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-02-25 16:10:03 +0100

    Built Login View.

--- 01_Erste-App-mit-SwiftUI/Erste-App-mit-SwiftUI/ContentView.swift
+++ 01_Erste-App-mit-SwiftUI/Erste-App-mit-SwiftUI/ContentView.swift
@@ -8,9 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var email : String = ""
+    @State var password : String = ""
+    
     var body: some View {
         VStack{
-            Text("asdasd")
+            Spacer()
+            
+            Group {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio( contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                Text("Shareii")
+                    .font(.title)
+                    .bold()
+                Text("Teile deine Fotos mit anderen")
+                    .foregroundColor(Color.gray)
+                    .font(.subheadline)
+                    .multilineTextAlignment(.center)
+            }
+            
+            Spacer()
+            Divider()
+            
+            HStack {
+                Image(systemName: "envelope.fill")
+                    .foregroundColor(Color.gray)
+                TextField("Email...", text: $email)
+            }
+            .padding()
+            .border(Color.init(white:0.9))
+            .padding()
+            
+            HStack {
+                Image(systemName: "lock.fill")
+                    .foregroundColor(Color.gray)
+                TextField("Passwort...", text: $password)
+            }
+            .padding()
+            .border(Color.init(white:0.9))
+            .padding()
+            
+            Button(
+                action: {},
+                label: {
+                HStack {
+                    Spacer()
+                    Text("Login")
+                        .foregroundColor(Color.white)
+                        .bold()
+                    Spacer()
+                }
+                .padding()
+                .background(.black)
+                .cornerRadius(5)
+                .shadow(radius: 10)
+                .padding()
+            })
+            
+            Spacer()
+            Divider()
+            
+            HStack {
+                Text("Du hast noch keinen Account?")
+                    .foregroundColor(Color.gray)
+                    .font(.footnote)
+                Button(
+                    action: {},
+                    label: {
+                    Text("Account erstellen")
+                        .foregroundColor(Color.black)
+                        .bold()
+                })
+            }
         }
     }
 }
