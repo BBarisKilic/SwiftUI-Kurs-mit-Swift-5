7dbf16f064c3382b004ad0d680b0b46355174f1e
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-03-13 13:17:39 +0100

    Added Class vs. Struct

--- 02_SwiftUI-Syntax-und-Grundlagen.playground/Contents.swift
+++ 02_SwiftUI-Syntax-und-Grundlagen.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// 1. Class vs. Struct
+
+class Player1 {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func printName() {
+        print(name)
+    }
+}
+
+struct Player2 {
+    var name: String
+    
+    func printName() {
+        print(name)
+    }
+}
+
+var player1 = Player1(name: "Hans")
+var player2 = Player2(name: "Andre")
+
+player1.printName()
+player2.printName()
+
+
