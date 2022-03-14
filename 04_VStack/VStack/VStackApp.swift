435460382daa040a9b32495af5a41a803283bbb6
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-03-14 23:15:53 +0100

    Added VStack

---
+++ 04_VStack/VStack/VStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  VStackApp.swift
+//  VStack
+//
+//  Created by Bülent Barış Kılıç on 14.03.22.
+//
+
+import SwiftUI
+
+@main
+struct VStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
