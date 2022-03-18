d466717a30b2a6066c03ab0e7611946567cb1819
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-03-18 15:59:17 +0100

    Created HStack

---
+++ 05_HStack/HStack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HStack
+//
+//  Created by Bülent Barış Kılıç on 18.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 05_HStack/HStack/HStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HStackApp.swift
+//  HStack
+//
+//  Created by Bülent Barış Kılıç on 18.03.22.
+//
+
+import SwiftUI
+
+@main
+struct HStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
