42284ba798bcfcd8781e8b6b92f5dde1b989fde7
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-05-07 03:08:36 +0300

    Added ZStack.

---
+++ 06_ZStack/ZStack/ZStack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZStack
+//
+//  Created by Bülent Barış Kılıç on 07.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 06_ZStack/ZStack/ZStack/ZStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZStackApp.swift
+//  ZStack
+//
+//  Created by Bülent Barış Kılıç on 07.05.22.
+//
+
+import SwiftUI
+
+@main
+struct ZStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
